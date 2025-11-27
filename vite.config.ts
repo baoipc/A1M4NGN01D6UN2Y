0369adf8b543dd1c4198411dafeecf7ae284d639
baoipc <baofam1906@gmail.com>
2025-11-27 19:17:42 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "A1M4NGN01D6UN2Y",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
